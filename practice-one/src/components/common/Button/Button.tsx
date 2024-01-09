3d7deed08cc9334d8b8af7fdd9ec71cee21906fe
Hien Duong <hien.duong@asnet.com.vn>
2024-01-09 14:08:02 +0700

    fix: udpate props and sytle component button

--- practice-one/src/components/common/Button/Button.tsx
+++ practice-one/src/components/common/Button/Button.tsx
@@ -1,20 +1,73 @@
-import classNames from 'classnames';
-import { BUTTON_VARIANT } from '../../../types/common';
+import React, { useCallback } from 'react';
 import styles from './index.module.css';
+import classNames from 'classnames';
 
-export interface ButtonProps {
-  children: string;
-  variant?: BUTTON_VARIANT;
-  className: string;
-  onClick?: () => void;
+interface ButtonProps {
+  children: React.ReactNode;
+  variant?: 'text' | 'outline' | 'filled' | 'filledTonal';
+  size?: 'small' | 'medium' | 'large';
+  type?: 'button' | 'submit' | 'reset';
+  color?: 'primary' | 'icon' | 'light';
+  disable?: boolean;
+  customClasses?: string;
+  startIcon?: React.ReactNode;
+  endIcon?: React.ReactNode;
+  onClick?: (e: React.MouseEvent<HTMLButtonElement>) => void;
 }
 
-const Button = ({ children, variant = BUTTON_VARIANT.PRIMARY, className, onClick }: ButtonProps) => {
-  const buttonClasses = classNames(styles[variant], className);
+/**
+ * Button component
+ * @param children Text for the button.
+ * @param variant Variant of the button. Can be one of 'text', 'outline', 'filled', or 'filledTonal.
+ * @param size Size of the button. Can be one of 'small', 'medium', or 'large'.
+ * @param type Type of the button. Can be one of 'button', 'submit', or 'reset'.
+ * @param color Color of the button. Can be one of 'primary', 'secondary', 'light'.
+ * @param disable Disable of the button. Can be one of 'true' to disable or 'false' to none disable.
+ * @param customClasses  Custom variant class for the button.
+ * @param startIcon To add icon before content button.
+ * @param endIcon To add icon after content button.
+ * @param onClick Click event handler function for the button.
+ * @returns {JSX.Element} - Button element.
+ */
+const Button = ({
+  children,
+  variant = 'text',
+  size = 'medium',
+  type = 'button',
+  color = 'light',
+  disable = false,
+  customClasses = '',
+  startIcon,
+  endIcon,
+  onClick = () => {},
+  ...restProps
+}: ButtonProps): JSX.Element => {
+  const handleButtonClick = useCallback(
+    (e: React.MouseEvent<HTMLButtonElement>) => {
+      onClick(e);
+    },
+    [onClick],
+  );
+
+  const buttonClasses = classNames([
+    styles.button,
+    styles[size],
+    styles[variant],
+    styles[color],
+    customClasses,
+  ]);
 
   return (
-    <button type="button" onClick={onClick} className={buttonClasses}>
+    <button
+      type={type === 'submit' ? 'submit' : type === 'reset' ? 'reset' : 'button'}
+      className={buttonClasses}
+      disabled={disable}
+      onClick={handleButtonClick}
+      {...restProps}
+    >
+      {startIcon && <span className="start-icon">{startIcon}</span>}
       {children}
+      {endIcon && <span className="end-icon">{endIcon}</span>}
     </button>
   );
 };
