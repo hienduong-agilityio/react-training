dd82d1674473373473ab92254f44ffb534bb3ee3
Hien Duong <hien.duong@asnet.com.vn>
2024-01-10 13:29:15 +0700

    fix: update class name for sidebar component

--- practice-one/src/components/layouts/Sidebar/Sidebar.stories.tsx
+++ practice-one/src/components/layouts/Sidebar/Sidebar.stories.tsx
@@ -1,5 +1,5 @@
 import type { Meta } from '@storybook/react';
-import { Sidebar, SidebarItem } from './Slidebar';
+import { Sidebar, SidebarItem } from './Sidebar';
 
 const meta = {
   title: 'Components/layouts/Sidebar',
