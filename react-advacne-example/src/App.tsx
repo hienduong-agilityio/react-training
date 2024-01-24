a2f3513787d4d36bb8f72fc7f135a2fdaf6dbb6b
Hien Duong <hien.duong@asnet.com.vn>
2024-01-24 11:30:14 +0700

    feat: update Skeleton for code splitting Suspense

--- react-advacne-example/src/App.tsx
+++ react-advacne-example/src/App.tsx
@@ -1,48 +1,55 @@
-import { useState } from "react";
+// import { useState } from "react";
 import "./App.css";
-import {
-  CallBackTutorial,
-  ParentComponent,
-} from "./hooks/DemoUseCallBack/DemoUseCallBack";
-import LearnCallBack from "./hooks/LearnReactHookUseCallBack";
-import { Content, Header, Sidebar } from "./hooks/LearnReactHookUseContext";
+// import {
+//   CallBackTutorial,
+//   ParentComponent,
+// } from "./hooks/DemoUseCallBack/DemoUseCallBack";
+// import LearnCallBack from "./hooks/LearnReactHookUseCallBack";
+// import { Content, Header, Sidebar } from "./hooks/LearnReactHookUseContext";
 
 // import MyComponent from "./hooks/DemoUseContext/MyContextComponent";
 // import MyContextProvider from "./hooks/DemoUseContext/MyContextProvider";
-import { AppProvider } from "./hooks/Context/AppContext";
-import ReducerTutorial from "./hooks/DemoUseReduces/ReducerTutorial ";
-import MemoTutorial from "./hooks/DemoUseMemo/MemoTutorial";
-import RefTutorial from "./hooks/DemoUseRef/RefTutorial";
+// import { AppProvider } from "./hooks/Context/AppContext";
+// import ReducerTutorial from "./hooks/DemoUseReduces/ReducerTutorial ";
+// import MemoTutorial from "./hooks/DemoUseMemo/MemoTutorial";
+// import RefTutorial from "./hooks/DemoUseRef/RefTutorial";
+// import DemoSuspenseCard from "./hooks/DemoSuspense/DemoSuspenseCard";
+import { Suspense, lazy } from "react";
+import DemoSuspenseCardSkeleton from "./hooks/DemoSuspense/DemoSuspenseSkeleton";
 
 function App() {
-  const [isOpen, setIsOpen] = useState(false);
+  // const [isOpen, setIsOpen] = useState(false);
+  const Card = lazy(() => import("./hooks/DemoSuspense/DemoSuspenseCard"));
 
   return (
     <div className="App">
       {/* Call back example */}
-      <CallBackTutorial />
-      <ParentComponent />
+      {/* <CallBackTutorial />
+      <ParentComponent /> */}
 
       {/* Context
       <MyContextProvider>
         <MyComponent />
       </MyContextProvider> */}
-      <LearnCallBack />
+      {/* <LearnCallBack /> */}
       {/* Any children in AppProvider can use state  */}
-      <AppProvider>
-        <>
-          {/* <Header isOpen={isOpen} setIsOpen={setIsOpen} userData={userData} />
+      {/* <AppProvider>
+        <> */}
+      {/* <Header isOpen={isOpen} setIsOpen={setIsOpen} userData={userData} />
           <Content isOpen={isOpen} />
           {isOpen && <Sidebar userData={userData} />} */}
-          <Header isOpen={isOpen} setIsOpen={setIsOpen} />
+      {/* <Header isOpen={isOpen} setIsOpen={setIsOpen} />
           <Content isOpen={isOpen} />
-          {isOpen && <Sidebar />}
-        </>
-      </AppProvider>
+          {isOpen && <Sidebar />} */}
+      {/* </>
+      </AppProvider> */}
 
-      <ReducerTutorial />
+      {/* <ReducerTutorial />
       <MemoTutorial />
-      <RefTutorial />
+      <RefTutorial /> */}
+      <Suspense fallback={<DemoSuspenseCardSkeleton />}>
+        <Card />
+      </Suspense>
     </div>
   );
 }
