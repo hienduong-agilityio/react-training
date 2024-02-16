952a0250290d8712b350f75e1d6ce45a76f6a3ac
Hien Duong <hien.duong@asnet.com.vn>
2024-02-16 13:51:52 +0700

    fix: update type for variable

--- practice-two/src/components/common/Button/index.tsx
+++ practice-two/src/components/common/Button/index.tsx
@@ -2,6 +2,10 @@ import React, { useCallback } from 'react';
 
 // Styles
 import classNames from 'classnames';
+
+// Constants
+import { BUTTON_VARIANT } from '../../../constants/common';
+
 interface IButtonProps {
   children: React.ReactNode;
   variant?: 'text' | 'outline' | 'filled' | 'filledTonal';
@@ -15,23 +19,23 @@ interface IButtonProps {
   onClick?: (e: React.MouseEvent<HTMLButtonElement>) => void;
 }
 
-const buttonDefault =
+const buttonDefault: string =
   'inline-flex rounded-xl font-medium uppercase flex gap-2 w-max items-center hover:opacity-80 active:shadow-buttonActive cursor-pointer disabled:opacity-60';
 
-const variantClasses = {
+const variantClasses: { [key in BUTTON_VARIANT]: string } = {
   text: ' bg-primary hover:bg-blue-700 border-blue-600 border hover:border-blue-700',
   outline: 'border border-primary',
   filled: 'bg-primary text-white rounded-lg',
   filledTonal: 'bg-primary text-white border border-primary'
 };
 
-const sizeClasses = {
+const sizeClasses: { [key in 'small' | 'medium' | 'large']: string } = {
   small: 'px-3 py-1 text-sm',
   medium: 'px-4 py-2 text-base',
   large: 'px-6 py-3 text-lg'
 };
 
-const colorClasses = {
+const colorClasses: { [key in 'default' | 'primary' | 'secondary']: string } = {
   default: '',
   primary: 'bg-primary',
   secondary: 'bg-secondary'
@@ -64,14 +68,15 @@ const Button = ({
   onClick = () => {},
   ...restProps
 }: IButtonProps): JSX.Element => {
-  const handleButtonClick = useCallback(
-    (e: React.MouseEvent<HTMLButtonElement>) => {
-      onClick(e);
-    },
-    [onClick]
-  );
+  const handleButtonClick: (e: React.MouseEvent<HTMLButtonElement>) => void =
+    useCallback(
+      (e: React.MouseEvent<HTMLButtonElement>) => {
+        onClick(e);
+      },
+      [onClick]
+    );
 
-  const buttonClasses = classNames([
+  const buttonClasses: string = classNames([
     buttonDefault,
     variantClasses[variant],
     sizeClasses[size],
