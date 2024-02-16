90e8e429cca2505688b92ae1db7af88f8b61e4f2
Hien Duong <hien.duong@asnet.com.vn>
2024-02-16 10:34:36 +0700

    fix: update config to commit

--- practice-two/src/App.tsx
+++ practice-two/src/App.tsx
@@ -1,12 +1,14 @@
 import Button from './components/common/Button';
 
 function App() {
+
   return (
     <>
       <h1 className="text-4xl font-bold underline">Hello world!</h1>
       <Button customClasses='bg-yellow-400 '>Hello</Button>
     </>
   );
+  
 }
 
 export default App;
