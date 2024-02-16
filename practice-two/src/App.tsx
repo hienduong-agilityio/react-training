c87580715932adfcd242bc009ecefa9ce343608e
Hien Duong <hien.duong@asnet.com.vn>
2024-02-16 10:36:38 +0700

    fix: update husky

--- practice-two/src/App.tsx
+++ practice-two/src/App.tsx
@@ -1,14 +1,12 @@
 import Button from './components/common/Button';
 
 function App() {
-
   return (
     <>
       <h1 className="text-4xl font-bold underline">Hello world!</h1>
-      <Button customClasses='bg-yellow-400 '>Hello</Button>
+      <Button customClasses="bg-yellow-400 ">Hello</Button>
     </>
   );
-  
 }
 
 export default App;
