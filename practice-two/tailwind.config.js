0ab7b3065318d2aead3b7b82030bf7bb12641307
Hien Duong <hien.duong@asnet.com.vn>
2024-02-16 10:31:18 +0700

    feat: custom variabe by tailwinds

--- practice-two/tailwind.config.js
+++ practice-two/tailwind.config.js
@@ -2,7 +2,20 @@
 export default {
   content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
   theme: {
-    extend: {}
+    extend: {
+      colors: {
+        primary: '#416edf',
+        secondary: '#ffce31',
+        white: '#ffffff',
+        light: '#f5f7fb',
+        danger: '#dc3545',
+        'gray-500': '#3333',
+        'gray-300': '#dcdcdc'
+      },
+      fontFamily: {
+        sans: ['Roboto', 'sans-serif']
+      }
+    }
   },
   plugins: []
 };
