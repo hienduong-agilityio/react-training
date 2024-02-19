4a277a23cbde89fdf6f998c265f19a265e23087e
Hien Duong <hien.duong@asnet.com.vn>
2024-02-19 09:19:05 +0700

    test: udpate test todo list and snapshot

---
+++ practice-two/src/components/common/Typography/Typography.test.tsx
@@ -0,0 +1,68 @@
+// Library
+import { render } from '@testing-library/react';
+
+// Components
+import Typography, { TEXT_SIZE } from './index';
+
+// Library
+import '@testing-library/jest-dom';
+
+describe('Typography Component', () => {
+  it('renders with default props', () => {
+    const { getByText } = render(<Typography>Hello World</Typography>);
+    const typographyElement = getByText('Hello World');
+    expect(typographyElement).toBeInTheDocument();
+    expect(typographyElement).toHaveClass('text-black font-normal text-base');
+  });
+
+  it('renders with custom color', () => {
+    const { getByText } = render(<Typography color="white">Hello World</Typography>);
+    const typographyElement = getByText('Hello World');
+    expect(typographyElement).toBeInTheDocument();
+    expect(typographyElement).toHaveClass('text-white font-normal text-base');
+  });
+
+  it('renders with custom size', () => {
+    const { getByText } = render(<Typography size={TEXT_SIZE.LARGE}>Hello World</Typography>);
+    const typographyElement = getByText('Hello World');
+    expect(typographyElement).toBeInTheDocument();
+    expect(typographyElement).toHaveClass('text-black font-normal text-5xl');
+  });
+
+  it('renders with custom classes', () => {
+    const { getByText } = render(<Typography customClasses="extra-class">Hello World</Typography>);
+    const typographyElement = getByText('Hello World');
+    expect(typographyElement).toBeInTheDocument();
+    expect(typographyElement).toHaveClass('text-black font-normal text-base extra-class');
+  });
+
+  it('renders different variants', () => {
+    const { getByText } = render(
+      <>
+        <Typography variant="h1">Heading 1</Typography>
+        <Typography variant="h2">Heading 2</Typography>
+        <Typography variant="h3">Heading 3</Typography>
+        <Typography variant="h4">Heading 4</Typography>
+        <Typography variant="h5">Heading 5</Typography>
+        <Typography variant="h6">Heading 6</Typography>
+        <Typography>Paragraph</Typography>
+      </>
+    );
+
+    expect(getByText('Heading 1').tagName).toBe('H1');
+    expect(getByText('Heading 2').tagName).toBe('H2');
+    expect(getByText('Heading 3').tagName).toBe('H3');
+    expect(getByText('Heading 4').tagName).toBe('H4');
+    expect(getByText('Heading 5').tagName).toBe('H5');
+    expect(getByText('Heading 6').tagName).toBe('H6');
+    expect(getByText('Paragraph').tagName).toBe('P');
+  });
+
+  it('renders correctly with default props', () => {
+    const { container } = render(
+      <Typography> Lorem ipsum dolor sit, amet consectetur adipisicing elit. Dolore, possimus!</Typography>
+    );
+    expect(container.firstChild).toBeTruthy();
+    expect(container.firstChild).toMatchSnapshot();
+  });
+});
