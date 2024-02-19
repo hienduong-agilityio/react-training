c1d5034edb41ea2500cd71b21a4eb42e4046ecdc
Hien Duong <hien.duong@asnet.com.vn>
2024-02-19 17:22:25 +0700

    feat: update color and style chip component

--- practice-two/src/components/common/Chip/index.tsx
+++ practice-two/src/components/common/Chip/index.tsx
@@ -1,28 +1,72 @@
+// Library
+import classNames from 'classnames';
 import { ReactNode } from 'react';
 
 export interface IChipProps {
-  color: '';
-  size: '';
-  icon: ReactNode;
+  color: CHIP_COLOR;
+  icon?: ReactNode;
   label: string;
+  customClasses?: string;
 }
+
+export enum CHIP_COLOR {
+  FIRE = 'fire',
+  BUG = 'bug',
+  WATER = 'water',
+  FLYING = 'flying',
+  POISON = 'poison',
+  NORMAL = 'normal',
+  ROCK = 'rock',
+  GROUND = 'ground',
+  FIGHTING = 'fighting',
+  GHOST = 'ghost',
+  PSYCHIC = 'psychic',
+  ICE = 'ice',
+  DRAGON = 'dragon',
+  DARK = 'dark',
+  FAIRY = 'fairy',
+  ELECTRIC = 'electric',
+  STEEL = 'steel',
+  GRASS = 'grass'
+}
+
+const chipDefault: string = 'inline-block rounded-full py-1 px- capitalize';
+
+const colorClasses: { [key in CHIP_COLOR]: string } = {
+  fire: 'bg-fire',
+  bug: 'bg-bug',
+  water: 'bg-water',
+  flying: 'bg-flying',
+  poison: 'bg-poison',
+  normal: 'bg-normal',
+  rock: 'bg-rock',
+  ground: 'bg-ground',
+  fighting: 'bg-fighting',
+  ghost: 'bg-ghost',
+  psychic: 'bg-psychic',
+  ice: 'bg-ice',
+  dragon: 'bg-dragon',
+  dark: 'bg-dark',
+  fairy: 'bg-fairy',
+  electric: 'bg-electric',
+  steel: 'bg-steel',
+  grass: 'bg-grass'
+};
+
 /**
  * Chip Component
  * @param color - The background color of the chip.
- * @param size - The size of the chip. Possible values: 'small', 'medium'.
- * @param variant - The style possible values: 'solid', 'plain', 'outlined', 'soft'.
  * @param icon - The icon to be displayed on the chip.
  * @param label - The content to be displayed inside the chip.
+ *
  * @returns {JSX.Element} - The Chip component.
  */
-const Chip = ({ color = '', size = '', icon, label }: IChipProps): JSX.Element => {
-  const chipStyles = {
-    backgroundColor: disabled ? 'gray' : color,
-    fontSize: size === 'small' ? '0.8rem' : '1rem'
-  };
+
+const Chip = ({ color = CHIP_COLOR.NORMAL, icon, label = '', customClasses = '' }: IChipProps): JSX.Element => {
+  const chipClasses: string = classNames([chipDefault, colorClasses[color], customClasses]);
 
   return (
-    <div className={chipClasses} style={chipStyles}>
+    <div className={chipClasses}>
       {icon && <span className="chip-icon">{icon}</span>}
       <span className="chip-label">{label}</span>
     </div>
--- practice-two/tailwind.config.js
+++ practice-two/tailwind.config.js
@@ -10,7 +10,25 @@ export default {
         light: '#f5f7fb',
         danger: '#dc3545',
         'gray-500': '#3333',
-        'gray-300': '#dcdcdc'
+        'gray-300': '#dcdcdc',
+        fire: '#ff6464',
+        bug: '#c9ff84',
+        water: '#9ff3ff',
+        flying: '#2299EE',
+        poison: '#d89cfd',
+        normal: '#cbcbcb',
+        rock: '#cfc06f',
+        ground: '#ffbf72',
+        fighting: '#ff699f',
+        ghost: '#b592ff',
+        psychic: '#ff5e60',
+        ice: '#aefff4',
+        dragon: '#87c5ff',
+        dark: '#8f8f8f',
+        fairy: '#ffa2e3',
+        electric: '#fffa86',
+        steel: '#a4ffe9',
+        grass: '#80e177'
       },
       fontFamily: {
         sans: ['Roboto', 'sans-serif']
