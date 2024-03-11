2bdd18e2c96b1c30c39213c6db4b499df4c4fbe8
Hien Duong <hien.duong@asnet.com.vn>
2024-03-11 11:28:06 +0700

    fix: udpated store

--- practice-two/src/components/layouts/Pokedex/index.tsx
+++ practice-two/src/components/layouts/Pokedex/index.tsx
@@ -28,15 +28,15 @@ export interface IPokemonData {
 
 const Pokedex = (): JSX.Element => {
   const [isOpen, setIsOpen] = useState(false);
-  const { data, loading, error, pokemonID } = usePokemonContext();
-  // Display loading indicator if data is still being fetched
-  if (loading) {
+  const { state } = usePokemonContext();
+  // Display state.loading indicator if data is still being fetched
+  if (state.loading) {
     return <span>Loading...</span>;
   }
 
   // Display error message if there was an issue fetching data
-  if (error) {
-    return <span>Error: {error}</span>;
+  if (state.error) {
+    return <span>Error: {state.error}</span>;
   }
 
   const handlePopup = () => {
@@ -46,7 +46,7 @@ const Pokedex = (): JSX.Element => {
   return (
     <section className="pt-24">
       <div className="grid justify-items-center sm:items-stretch grid-cols-1 sm:grid-cols-2 md:grid-cols-3 xl:grid-cols-4 gap-x-8 gap-y-20">
-        {data?.map((pokemon: IPokemonData) => (
+        {state.data?.map((pokemon: IPokemonData) => (
           <PokemonCard
             key={pokemon.id}
             pokemonID={pokemon.id}
@@ -59,7 +59,7 @@ const Pokedex = (): JSX.Element => {
       </div>
       <div>
         <Popup isOpen={isOpen} onClosePopup={handlePopup}>
-          {data && <PokemonDetails pokemonData={data[Number(pokemonID) - 1]} />}
+          {state.data && <PokemonDetails pokemonData={state.data[Number(state.pokemonID) - 1]} />}
         </Popup>
       </div>
     </section>
