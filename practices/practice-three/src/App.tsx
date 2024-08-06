a3e5f64276b5e4a727ff6c5e12580ffa64165659
Hien The Duong <hien.duong@asnet.com.vn>
2024-08-06 10:57:02 +0700

    Implement sidebar component

--- practices/practice-three/src/App.tsx
+++ practices/practice-three/src/App.tsx
@@ -1,11 +1,22 @@
 // Components
 import ProjectTable from '@/components/ProjectTable';
+import Sidebar from '@/components/layout/SideBar';
 
 // Data
 import PROJECT_ITEMS from '../database/data.json';
 
-const App = () => {
-  return <ProjectTable dataTable={PROJECT_ITEMS}></ProjectTable>;
+/**
+ * The App component as the main view for the application.
+ *
+ * @returns {JSX.Element} The main structure of the application with a sidebar and project table.
+ */
+const App = (): JSX.Element => {
+  return (
+    <main className='flex'>
+      <Sidebar />
+      <ProjectTable dataTable={PROJECT_ITEMS} />
+    </main>
+  );
 };
 
 export default App;
