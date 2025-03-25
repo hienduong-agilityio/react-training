db38c82deee259293f54bc0ec4e60187dff568a5
HienDuong <hien.duong@asnet.com.vn>
2025-03-25 14:38:47 +0700

    fix: update app content

--- examples-demo/react-19-training/src/App.tsx
+++ examples-demo/react-19-training/src/App.tsx
@@ -2,7 +2,6 @@ import { useState } from 'react';
 import reactLogo from './assets/react.svg';
 import viteLogo from '/vite.svg';
 import './index.css';
-import { Blog } from './Blog';
 
 function App() {
   const [count, setCount] = useState(0);
@@ -25,7 +24,6 @@ function App() {
         </p>
       </div>
       <p className='read-the-docs'>Click on the Vite and React logos to learn more</p>
-      <Blog title='hi' content='hello' />
     </>
   );
 }
