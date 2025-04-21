2341c2c10a77120238128cc84ff84735fc035a8d
HienDuong <hien.duong@asnet.com.vn>
2025-04-21 17:02:05 +0700

    fix: update react scan

--- practices/practice-four/src/components/common/ToastMessage/index.tsx
+++ practices/practice-four/src/components/common/ToastMessage/index.tsx
@@ -1,5 +1,5 @@
 // Libraries
-import { useState, useEffect, memo } from 'react';
+import { useRef, memo, useDeferredValue } from 'react';
 import { Link } from 'react-router-dom';
 import classNames from 'classnames';
 
@@ -11,17 +11,10 @@ import errorIcon from '@public/images/errorIcon.svg';
 import { ROUTE } from '@/constants/route';
 
 export interface IToastProps {
-  // onClose: Callback function triggered when the message is closed.
-  onClose: () => void;
-  // children: The content to be displayed within the message.
+  onClose?: () => void;
   children: React.ReactNode;
-  // title: Optional title for the message.
-  title?: string;
-  // undoEnabled: Enables or disables the 'Undo' option within the message.
   undoEnabled?: boolean;
-  // timeoutDuration: The duration (in milliseconds) before the message automatically disappears.
   timeoutDuration?: number;
-  // type: Specifies the type of message, either 'success' or 'error'.
   type?: 'success' | 'error';
 }
 
@@ -30,51 +23,58 @@ export interface IToastProps {
  *
  * @returns {JSX.Element | null} - Toast message element or null if not visible
  */
-const Toast = ({
-  onClose = () => {},
-  children,
-  undoEnabled = false,
-  timeoutDuration = 5000,
-  type = 'success'
-}: IToastProps): JSX.Element | null => {
-  const [isVisible, setIsVisible] = useState(true);
+const Toast = memo(
+  ({
+    onClose = () => {},
+    children,
+    undoEnabled = false,
+    timeoutDuration = 5_000,
+    type = 'success'
+  }: IToastProps): JSX.Element | null => {
+    const isVisibleRef = useRef(true);
+    const timerRef = useRef<number>(0);
+    const deferredType = useDeferredValue(type);
 
-  useEffect(() => {
-    const timer = setTimeout(() => {
-      setIsVisible(false);
-      onClose();
-    }, timeoutDuration);
+    const attachToast: React.RefCallback<HTMLDivElement> = (node) => {
+      if (!node) return;
 
-    return () => {
-      clearTimeout(timer);
+      timerRef.current = window.setTimeout(() => {
+        isVisibleRef.current = false;
+        onClose();
+      }, timeoutDuration);
+
+      return () => {
+        clearTimeout(timerRef.current);
+      };
     };
-  }, [onClose, timeoutDuration]);
 
-  const containerClass = classNames(
-    'flex items-center divide-x rtl:divide-x-reverse p-4 w-full max-w-xs rounded-lg shadow',
-    {
-      'text-success-100 bg-success-800 divide-green-950': type === 'success',
-      'text-red-100 bg-red-800 divide-red-950': type === 'error'
-    }
-  );
+    const containerClass = classNames(
+      'flex items-center divide-x rtl:divide-x-reverse p-4 w-full max-w-xs rounded-lg shadow',
+      {
+        'text-success-100 bg-success-800 divide-green-950': deferredType === 'success',
+        'text-red-100 bg-red-800 divide-red-950': deferredType === 'error'
+      }
+    );
 
-  const icon = type === 'success' ? successIcon : errorIcon;
+    const icon = deferredType === 'success' ? successIcon : errorIcon;
 
-  return isVisible ? (
-    <div className={containerClass}>
-      <div className='text-sm font-normal inline-flex items-center gap-3 pl-1'>
-        <img src={icon} alt={type} />
-        {children}
-      </div>
-      {undoEnabled && (
-        <div className='flex items-center ml-4 pl-4 space-x-2 rtl:space-x-reverse'>
-          <Link to={ROUTE.PROJECT} className='text-sm font-medium hover:underline'>
-            Undo
-          </Link>
+    return isVisibleRef.current ? (
+      <div ref={attachToast} className={containerClass}>
+        <div className='text-sm font-normal inline-flex items-center gap-3 pl-1'>
+          <img src={icon} alt={deferredType} />
+          {children}
         </div>
-      )}
-    </div>
-  ) : null;
-};
 
-export default memo(Toast);
+        {undoEnabled && (
+          <div className='flex items-center ml-4 pl-4 space-x-2 rtl:space-x-reverse'>
+            <Link to={ROUTE.PROJECT} className='text-sm font-medium hover:underline'>
+              Undo
+            </Link>
+          </div>
+        )}
+      </div>
+    ) : null;
+  }
+);
+
+export default Toast;
