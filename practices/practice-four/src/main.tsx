52770d6d3a02090679e97a145050bd51c802ca35
HienDuong <hien.duong@asnet.com.vn>
2025-04-24 10:21:46 +0700

    feat: updaet react scan config

--- practices/practice-four/src/main.tsx
+++ practices/practice-four/src/main.tsx
@@ -1,7 +1,7 @@
 // Libraries
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { scan } from 'react-scan'; // must be imported before React and React DOM
+import { Options, scan } from 'react-scan'; // must be imported before React and React DOM
 
 // Components
 import App from '@/App.tsx';
@@ -10,8 +10,13 @@ import App from '@/App.tsx';
 import '@/index.css';
 
 scan({
-  enabled: true
-});
+  enabled: import.meta.env.DEV, // Enable only dev
+  log: true, // Log debug
+  trace: true,
+  highlight: true, // Highlight component re-render
+  name: true, // component re-render
+  props: true // Prop make to re-render
+} as Options);
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
