ba15c4422680e23385c807408771b5fc26ab6b80
HienDuong <hien.duong@asnet.com.vn>
2025-05-06 15:59:18 +0700

    feat: create proteced routes

--- examples-demo/clerk-react-training/src/App.tsx
+++ examples-demo/clerk-react-training/src/App.tsx
@@ -2,15 +2,36 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Dashboard from "./pages/Dashboard";
 import Login from "./pages/Login";
 import Signup from "./pages/Signup";
+import { SignedIn, SignedOut } from "@clerk/clerk-react";
+import Layout from "./layouts/Layout";
+import UserLayout from "./layouts/UserLayout";
 
 export default function App() {
   return (
     <Router>
       <Routes>
-        <Route path="login" element={<Login />} />
-        <Route path="signup" element={<Signup />} />
+        <Route
+          path="/"
+          element={
+            <SignedOut>
+              <Layout />
+            </SignedOut>
+          }
+        >
+          <Route path="login" element={<Login />} />
+          <Route path="signup" element={<Signup />} />
+        </Route>
         <Route path="register" element={""} />
-        <Route path="dashboard" element={<Dashboard />} />
+        <Route
+          path="dashboard"
+          element={
+            <SignedIn>
+              <UserLayout />
+            </SignedIn>
+          }
+        >
+          <Route path="" element={<Dashboard />} />
+        </Route>
       </Routes>
     </Router>
   );
